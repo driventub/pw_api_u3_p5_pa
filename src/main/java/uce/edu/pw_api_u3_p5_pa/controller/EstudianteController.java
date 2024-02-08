b78d979aed348478be1dcc0318beec36474744fb
driven <aldazpaulo@gmail.com>
2024-02-07 20:37:07 -0500

    consumes and produces

--- src/main/java/uce/edu/pw_api_u3_p5_pa/controller/EstudianteController.java
+++ src/main/java/uce/edu/pw_api_u3_p5_pa/controller/EstudianteController.java
@@ -3,6 +3,10 @@ package uce.edu.pw_api_u3_p5_pa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,13 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
 import uce.edu.pw_api_u3_p5_pa.repository.modelo.Estudiante;
 import uce.edu.pw_api_u3_p5_pa.service.IEstudianteService;
 
 @RestController
 @RequestMapping("/estudiantes")
-@Slf4j
 public class EstudianteController {
 
     @Autowired
@@ -34,26 +36,27 @@ public class EstudianteController {
      * @param nuevo: un valor de prueba
      * @return
      */
-    @GetMapping(path = "{id}")
-    public Estudiante consultarPorId(@PathVariable Integer id, @PathVariable(required = false) String nuevo) {
-        log.info(nuevo);
-        return this.service.seleccionar(id);
+    @GetMapping(path = "{id}", produces = "application/xml")
+    public ResponseEntity<Estudiante> consultarPorId(@PathVariable Integer id) {
+
+        // 240: recurso Estudiante enconntrado Satisfactoriamente!
+        return ResponseEntity.status(HttpStatus.OK).body(this.service.seleccionar(id));
     }
 
-    @PostMapping()
+    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
     public void guardar(@RequestBody Estudiante entity) {
 
         this.service.insertar(entity);
 
     }
 
-    @PutMapping(path = "{id}")
+    @PutMapping(path = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void actualizar(@RequestBody Estudiante estu, @PathVariable Integer id) {
         estu.setId(id);
         this.service.actualizar(estu);
     }
 
-    @PatchMapping("{id}")
+    @PatchMapping(path = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void actualizarParcial(@RequestBody Estudiante estu, @PathVariable Integer id) {
         estu.setId(id);
         this.service.actualizarParcial(estu.getApellido(), estu.getNombre(), estu.getId());
@@ -71,10 +74,15 @@ public class EstudianteController {
      * @param genero
      * @return
      */
-    @GetMapping()
-    public List<Estudiante> consultarTodos(@RequestParam(required = false, defaultValue = "M") String genero) {
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<List<Estudiante>> consultarTodos(
+            @RequestParam(required = false, defaultValue = "F") String genero) {
 
-        return this.service.seleccionarTodos(genero);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("mensaje_242", "Lista consultada de manera satisfactoria");
+        headers.add("El genero es", genero);
+
+        return new ResponseEntity<>(this.service.seleccionarTodos(genero), headers, 242);
     }
 
 }
