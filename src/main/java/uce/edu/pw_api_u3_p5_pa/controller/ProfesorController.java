7048cb4656abb1e72ac9002850a516166f21ad6f
driven <aldazpaulo@gmail.com>
2024-02-08 16:40:53 -0500

    richardson 2

--- src/main/java/uce/edu/pw_api_u3_p5_pa/controller/ProfesorController.java
+++ src/main/java/uce/edu/pw_api_u3_p5_pa/controller/ProfesorController.java
@@ -5,19 +5,23 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import uce.edu.pw_api_u3_p5_pa.repository.modelo.Profesor;
 import uce.edu.pw_api_u3_p5_pa.service.IProfesorService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/profesores")
@@ -26,15 +30,22 @@ public class ProfesorController {
     @Autowired
     private IProfesorService service;
 
-    @GetMapping(path = "{id}")
-    public Profesor consultarPorId(@PathVariable Integer id) {
-        return this.service.buscarPorId(id);
+    @GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Profesor> consultarPorId(@PathVariable Integer id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("El id es", Integer.toString(id));
+
+        return  new ResponseEntity<>(this.service.buscarPorId(id), headers, 252);
     }
 
     
-    @GetMapping()
-    public List<Profesor> consultarPorContrataciones(@RequestParam LocalDateTime fecha) {
-        return this.service.buscarContrataciones(fecha);
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Profesor>> consultarPorContrataciones(@RequestParam LocalDateTime fecha) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("mensaje_252", "Lista consultada de manera satisfactoria");
+        
+
+        return new ResponseEntity<>(this.service.buscarContrataciones(fecha), headers, 252);
     }
 
     /**
@@ -43,30 +54,59 @@ public class ProfesorController {
      * @param valor
      * @return
      */
-    @GetMapping(path = "/salarios/{valor}")
-    public Long contarSalarios(@PathVariable BigDecimal valor) {
-        return this.service.contarSalario(valor);
+    @GetMapping(path = "/salarios/{valor}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Long> contarSalarios(@PathVariable BigDecimal valor) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("mensaje_252", "Salario contado de manera satisfactoria");
+        headers.add("El genero es", valor.toString());
+
+        return new ResponseEntity<>(this.service.contarSalario(valor), headers, 252);
     }
 
-    @PostMapping()
-    public void ingresar(@RequestBody Profesor entity) {
-        this.service.insertar(entity);
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE )
+    public ResponseEntity<String> ingresar(@RequestBody Profesor entity) {
+        try {
+            this.service.insertar(entity);
+            return ResponseEntity.ok("Profesor insertado correctamente"); // 200 OK
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al insertar al Profesor: " + e.getMessage()); // 500 Internal Server Error
+        }
 
     }
 
-    @PutMapping()
-    public void actualizar(@RequestBody Profesor entity) {
-        this.service.actualizar(entity);
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> actualizar(@RequestBody Profesor entity) {
+        try {
+            this.service.actualizar(entity);
+            return ResponseEntity.ok("Profesor actualizado correctamente"); // 200 OK
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+                    .body("Metodo no valido: " + e.getMessage()); // 500 Internal Server Error
+        }
     }
 
     @PatchMapping()
-    public void actualizarSalario(@RequestParam Integer id, @RequestParam BigDecimal salario) {
-        this.service.actualizarSalario(id, salario);
+    public ResponseEntity<String> actualizarSalario(@RequestParam Integer id, @RequestParam BigDecimal salario) {
+        
+        try {
+            this.service.actualizarSalario(id, salario);
+            return ResponseEntity.ok("Salario actualizado correctamente"); // 200 OK
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al actualizar el Profesor: " + e.getMessage()); // 500 Internal Server Error
+        }
     }
 
     @DeleteMapping(path = "{id}")
-    public void eliminar(@PathVariable Integer id) {
-        this.service.eliminar(id);
+    public ResponseEntity<String> eliminar(@PathVariable Integer id) {
+        try {
+            this.service.eliminar(id);
+            return ResponseEntity.ok("Profesor correctamente eliminado"); // 200 OK
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al eliminar el Profesor: " + e.getMessage()); // 500 Internal Server Error
+        }
     }
 
 }
