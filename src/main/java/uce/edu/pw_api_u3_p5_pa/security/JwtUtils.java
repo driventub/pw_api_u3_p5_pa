489130a3ab557d7305e199e0590787ea68cd4457
driven <aldazpaulo@gmail.com>
2024-02-25 00:07:18 -0500

    cambio de jwt utils

--- src/main/java/uce/edu/pw_api_u3_p5_pa/security/JwtUtils.java
+++ src/main/java/uce/edu/pw_api_u3_p5_pa/security/JwtUtils.java
@@ -3,29 +3,32 @@ package uce.edu.pw_api_u3_p5_pa.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class JwtUtils {
 
-    @Value("${app.jwtSemilla}")
-    private String jwtSemilla;
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
 
-
-    public boolean validateJwtToken(String authToken){
-        try {
-            Jwts.parser().setSigningKey(this.jwtSemilla).parseClaimsJws(authToken);    
+    public boolean validateJwtToken(String token) {
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
             return true;
-        } catch (Exception e) {
-            log.error("Error" , e);
+        }catch(ExpiredJwtException e){
+            log.error(" Token expirado: {}",e.getMessage());
         }
-        
+        catch(SignatureException e){
+            log.error(" Token invalido: {}",e.getMessage());
+        }
         return false;
     }
 
     public String getUsernameFromJwtToken(String token){
-        return  Jwts.parser().setSigningKey(this.jwtSemilla).parseClaimsJws(token).getBody().getSubject();
+        return  Jwts.parser().setSigningKey(this.jwtSecret).parseClaimsJws(token).getBody().getSubject();
     }
 }
